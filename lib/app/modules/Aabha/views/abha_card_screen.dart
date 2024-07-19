9ef7edff4b2b6fdf4cf0b3b5383fa82c5b6ddeff
hitesh parmar <hiteshparmar16320@gmail.com>
2024-07-19 16:30:06 +0530

    changes:crated abha card screen files

---
+++ lib/app/modules/Aabha/views/abha_card_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AbhaCardScreen extends StatelessWidget {
+  const AbhaCardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/app/modules/Aabha/views/abha_provider_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AbhaProvidersScreen extends StatelessWidget {
+  const AbhaProvidersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
